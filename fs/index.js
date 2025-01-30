018e5a411ac1d8eebbc7e92d7a881d2319eafe83
krrishmittal <mittalkrrish0811@gmail.com>
2025-01-30 23:45:59 +0530

    file reader method

---
+++ fs/index.js
@@ -0,0 +1,9 @@
+// console.log("krrish")
+
+const fs=require("fs")
+// let data = fs.readFileSync("./krrish.txt", "utf8");
+// console.log(data)
+
+// fs.writeFileSync("pokemon.txt","i choose you")
+
+fs.appendFileSync("./pokemon.txt"," pikachu")
